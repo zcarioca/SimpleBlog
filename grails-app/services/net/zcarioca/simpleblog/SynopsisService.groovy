28ba8517b13bdb3387b3bf4c1e1364df4c0dcdda
zcarioca <zcarioca@zcarioca.net>
2013-05-14 23:35:21 -0400

    Fixed issues with PRE tags in the markup

--- grails-app/services/net/zcarioca/simpleblog/SynopsisService.groovy
+++ grails-app/services/net/zcarioca/simpleblog/SynopsisService.groovy
@@ -1,32 +1,45 @@
 package net.zcarioca.simpleblog
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 import com.petebevin.markdown.MarkdownProcessor
 
 class SynopsisService {
    static transactional = false
    
    public String makeHTML(String content) {
+      MarkdownProcessor mkp = new MarkdownProcessor()
       def matcher = content =~ /```\w+\s+/
+      int start = 0
+      StringBuilder mkout = new StringBuilder()
+      def preList = []
       
-//      println "============================================="
-//      println "OLD CONTENT: ${content}"
       while (matcher.find()) {
          def item = matcher.group()
          def lang = item.trim()[3..-1]
          
-//         println "Search: ${item}"
-//         println "Lang: ${lang}"
-
-         def idx = content.indexOf(item)
-         content = content.replaceFirst(item, "<pre><code class=\"${lang}\">")
-         content = content.replaceFirst("\\s+```", "</code></pre>")
+         int groupPoint = content.indexOf(item, start)
+         mkout << content.substring(start, groupPoint)
+         mkout << "{{INSERT POINT}}"
+         
+         int nextPoint = groupPoint + item.length()
+         start = content.indexOf("```", nextPoint)
+         
+         def codeblock = content.substring(nextPoint, start)
+         codeblock = StringEscapeUtils.escapeHtml(codeblock).replaceAll('\\$', '&#36;')
+         preList << "<pre><code class=\"${lang}\">${codeblock}</code></pre>"
+         start += 3
       }
-      content = new MarkdownProcessor().markdown(content)
-//      println "============================================="
-//      println "============================================="
-//      println "NEW CONTENT: ${content}"
-//      println "============================================="
-      return content
+      
+      mkout << content.substring(start)
+      
+      def output = mkp.markdown(mkout.toString())
+      
+      preList.each { string ->
+         def regex = /(<p>\s*)?\{\{INSERT POINT\}\}(\s*<\/p>)?/
+         output = output.replaceFirst(regex, string)
+      }
+      return output.toString()
    }
 
    public String makeSynopsis(String content) {
